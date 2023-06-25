27d7049afa37ed5ef4b2c8871d9647cee6b74540
Yuriy Pilipenko <yurik2061@gmail.com>
2023-06-25 21:38:59 +0300

    Update Filter.styled.jsx

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -5,11 +5,13 @@ export const FilterLabel = styled.label`
  color:  ${props => props.theme === 'light' ? '#444' : '#eee'};
 
   & >input{
+      
 
     &:disabled {
       border: none;
       outline: none;
       cursor: none;
+
  
       background-color: var(--chamomile);
       &::placeholder {
