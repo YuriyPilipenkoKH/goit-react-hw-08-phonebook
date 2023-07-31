d00b1d6fb915a30576e242e3d3c9c22b76714d21
Yuriy Pilipenko <yurik2061@gmail.com>
2023-07-31 13:15:26 +0300

    go

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
+import { Translation } from "react-i18next";
 import { HomeTitle, HomeWrapper } from "./Pages.styled";
+import CommentComponent from "components/CommentComponent/CommentComponent";
 
 
 export default function Home() {
@@ -9,6 +11,17 @@ export default function Home() {
          React-phonebook  
         </HomeTitle>
         <span>web application that allows users to store and manage their contacts, <br /> providing features for adding, editing, and deleting contact information</span>
+
+        <div>
+      <h1>
+        <Translation>{(t) => t("appTitle")}</Translation>
+      </h1>
+      <p>
+        <Translation>{(t) => t("welcomeMessage")}</Translation>
+      </p>
+    </div>
+
+    <CommentComponent></CommentComponent>
 
       </HomeWrapper>
     );
