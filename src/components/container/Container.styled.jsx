cf94cd2a6584ee17e26a13c57292678c202e229d
Yuriy Pilipenko <yurik2061@gmail.com>
2023-08-18 07:36:44 +0300

    news

--- src/components/container/Container.styled.jsx
+++ src/components/container/Container.styled.jsx
@@ -16,3 +16,24 @@ export const MainContainer = styled.div`
     margin: 0 auto;
    
 `
+
+// export const MainContainer = styled.div`
+//     display: grid;
+//     place-items: center;
+//     /* justify-items: center; */
+//     grid-gap: 25px;
+//     background-color: #777;
+//     padding: 0 20px;
+//     margin: 0 auto;
+//     outline: 1px solid crimson;
+//     width: 320px;
+
+//       @media screen and (min-width: 768px) {
+//       padding: 0 32px;
+//       width: 768px;
+//     }
+
+//       @media screen and (min-width: 1280px) {
+//         padding: 0 16px;
+//         width: 1280px;
+//     }
