d5930bf40f7691e2d75abd454d8b508c83278733
YuriyPilipenkoKH <113132127+YuriyPilipenkoKH@users.noreply.github.com>
2023-09-03 08:47:02 +0000

    Deploying to gh-pages from @ YuriyPilipenkoKH/goit-react-hw-08-phonebook@a3f67bf9478c7ba5d34593855a99a2b67ef4e72b 🚀

---
+++ static/js/415.1dde3860.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_hw_08_phonebook=self.webpackChunkreact_hw_08_phonebook||[]).push([[415],{5415:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var c=t(9439),r=t(1215),s=t(8820),u=t(2791),a=t(9434),p=t(6351),h=t(3329);function i(){var e=(0,u.useState)(s.v),n=(0,c.Z)(e,2),t=n[0],i=n[1],o=(0,a.v9)(p.VQ);return(0,u.useEffect)((function(){i("english"===o?s.o:s.v)}),[o]),(0,h.jsxs)(r.pp,{children:[(0,h.jsxs)(r.eA,{children:["Advanced ",t.appTitle]}),(0,h.jsx)("span",{children:t.appUnderTitle})]})}}}]);
+//# sourceMappingURL=415.1dde3860.chunk.js.map
