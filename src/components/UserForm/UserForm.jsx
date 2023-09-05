c226b05db92b8dbf3c8bff3392acf4dddff03d1e
Yuriy Pilipenko <yurik2061@gmail.com>
2023-09-05 14:38:57 +0300

    desk + innput

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
-import { BtnWrap, FormContainer, FormEditor, FormInput, FormLabel, FormStyled, ImgWrap } from "./UserForm.styled"
+import { Avatar, BtnWrap, Check, ConfirmButtonWrap, Cross, EditButton, FormContainer, FormEditor, FormInput, FormLabel, FormStyled, ImageControls, ImgWrap } from "./UserForm.styled"
 import { IconCrossForSearch, iconPen } from "utils/svgIcons";
-
+import avatarDefault2x from '../../img/Photo_default@2x.jpg';
+import { langEN, langUA } from "utils/languages";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getLang } from "redux/selectors";
+import { ReactComponent as Camera } from '../../img/camera.svg'
 
 
  
@@ -14,7 +19,16 @@ export const UserForm = () => {
         phone: '',
         location: '',
       });
-    
+      const [userPhoto, setUserPhoto] = useState('');  
+      const [edit, setEdit] = useState(false);
+      const language = useSelector(getLang)
+      const [lang, setLang] = useState(langUA);
+  
+      useEffect(() => {
+          setLang(language === 'english' ? langEN : langUA);
+      }, [language]);
+  
+
       const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({
@@ -41,12 +55,95 @@ export const UserForm = () => {
         setShowData(!showData);
         
     };
+
+    const handleClickInput = e => {
+        setEdit(true);
+        const [file] = e.target.files;
+        if (file) {
+            setUserPhoto(file);
+        }
+    };
+    const handleAddAvatar = () => {
+        const avatar = userPhoto;
+        const formData = new FormData();
+        for (const key in { avatar }) {
+            formData.append(key, { avatar }[key]);
+        }
+      
+        setEdit(false);
+        setUserPhoto(false);
+    };
+
+    const handleCancelAvatar = e => {
+        setUserPhoto();
+        setEdit(false);
+    }
+    const handleEditBtn = () => {
+        document.getElementById('userPhoto').click();
+    };
   return (
     <FormContainer>
-        <ImgWrap className="ImgWrap">
-            <img alt='alt'/>
-            <button>edit photo</button>
-        </ImgWrap>
+            <ImgWrap className="UserPhotoWrap">
+                <label htmlFor="userPhoto" >
+                    {!userPhoto &&  (
+                        <Avatar src={avatarDefault2x} alt="user avatar" />
+                    )}
+                    {/* {!userPhoto  (
+                        <Avatar src={user.avatarURL} alt="user avatar" />
+                    )} */}
+                    {!!userPhoto && (
+                        <Avatar
+                            src={URL.createObjectURL(userPhoto)}
+                            id="image"
+                            alt={userPhoto.username}
+                        />
+                    )}
+
+                    <input  
+                        type="file"
+                        id="userPhoto"
+                        name="userPhoto"
+                        accept=".png, .jpg, .jpeg, .webp"
+                        hidden
+                        disabled={!showData}
+                        value=""
+                        onChange={handleClickInput}
+                    />
+
+                    {/* <ErrorMessage name="user-avatar" component="div" /> */}
+                </label>
+
+                {showData && (
+                    <ImageControls>
+                        {edit && userPhoto ? (
+                            <ConfirmButtonWrap>
+                                <EditButton
+                                    type="button"
+                                    onClick={handleAddAvatar}
+                                >
+                                    <Check stroke="#00C3AD" />
+                                    {lang.confirm}
+                                </EditButton>
+                                <EditButton
+                                    type="button"
+                                    onClick={handleCancelAvatar}
+                                >
+                                    <Cross />
+                                    {lang.cancel}
+                                </EditButton>
+                            </ConfirmButtonWrap>
+                        ) : (
+                            <EditButton type="button" onClick={handleEditBtn}>
+                                <Camera
+                                    stroke="#54ADFF"
+                                    style={{ marginRight: 8 }}
+                                />
+                                {lang.edit}
+                            </EditButton>
+                        )}
+                    </ImageControls>
+                )}
+            </ImgWrap>
         <FormStyled onSubmit={handleSubmit}>
            <FormLabel htmlFor="name">Name:
           <FormInput
