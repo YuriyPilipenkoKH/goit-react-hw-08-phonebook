c91235012b076e720d09d93fcb9dbc50949badb2
Yuriy Pilipenko <yurik2061@gmail.com>
2023-09-30 22:37:52 +0300

    tooltip style

--- src/components/Tooltip/Tooltip.styled.jsx
+++ src/components/Tooltip/Tooltip.styled.jsx
@@ -1,28 +1,29 @@
 import styled from "styled-components";
 
 export const StyledTooltip = styled.div`
-    position: relative;
+  position: relative;
   display: inline-block;
 
   &:hover .tooltip-text {
     visibility: visible;
     opacity: 1;
-  }
+}
+
 `;
 
 export const TooltipText = styled.span`
-    visibility: hidden;
+  visibility: hidden;
   width: 150px;
   background-color: #333;
   color: #fff;
   text-align: center;
   border-radius: 4px;
-  padding: 5px;
+  padding: 8px;
   position: absolute;
   z-index: 1;
   bottom: 125%; /* Position above the element */
   left: 50%;
   transform: translateX(-50%);
   opacity: 0;
-  transition: opacity 0.3s;
+  transition: opacity 0.5s ease;
 `;
