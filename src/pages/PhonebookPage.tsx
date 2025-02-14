a6a95e2d00097a6404764891e14c28e4a045608e
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-14 16:10:02 +0200

    PhonebookWrapper

--- src/pages/PhonebookPage.tsx
+++ src/pages/PhonebookPage.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { PhonebookWrapper } from './Pages.styled'
+import { useLanguage } from '../hooks/useLanguage'
+import { useSelector } from 'react-redux'
+import { getLang } from '../redux/selectors/selectors'
 
 const PhonebookPage = () => {
+  const language = useSelector(getLang)
+  const lang = useLanguage()
   return (
-    <div>PhonebookPage</div>
+    <PhonebookWrapper className="phonebook__wrap">
+    {/* <Section title={lang.phonebook} icon ={language === 'english' &&   iconMphone}>
+      <ContactForm  />
+
+      <Filter />
+      
+      {contacts.length > 0 && (
+        <>
+       <ListBar></ListBar>
+        <ContactList  />
+        </>
+      )}
+    </Section> */}
+    
+
+  </PhonebookWrapper>
   )
 }
 
