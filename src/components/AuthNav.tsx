94d14dbc7109ac5b438a12b43da93795ddd5fe22
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-14 12:40:40 +0200

    authnav

---
+++ src/components/AuthNav.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useAuth } from '../hooks/useAuth';
+import { useLanguage } from '../hooks/useLanguage';
+import { StyledWrap } from './navigation/Navigation.styled';
+import { StyledLink } from './layout/Layout.styled';
+
+const AuthNav = () => {
+  const {token} = useAuth()
+  const lang = useLanguage()
+  return (
+    <StyledWrap>
+     {token && <StyledLink  to="/register">
+     {lang.regBtn}
+      </StyledLink>}
+      <StyledLink  to="/login">
+      {lang.logBtn}
+      </StyledLink>
+    </StyledWrap>
+  );
+};
+
+export default AuthNav
