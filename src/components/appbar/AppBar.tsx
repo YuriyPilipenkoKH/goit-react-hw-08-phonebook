bf947308276af20e8015ca5fdbfe447bf166e8cf
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-14 05:04:16 +0200

    nav styles

--- src/components/appbar/AppBar.tsx
+++ src/components/appbar/AppBar.tsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import {MdOutlineNightlight} from 'react-icons/md';
+import {MdOutlineLightMode} from 'react-icons/md';
+import { LangBtn, StyledHeader, ThemeBtn } from './AppBar.styled';
 
 const AppBar = () => {
+
+  // const { isLoggedIn } = useAuth();
+  // const theme = useSelector(getTheme)
+
+  // const dispatch = useDispatch()
+  // const language = useSelector(getLang)
+
+  // const themeInvertor =(theme) => {
+  //  return  theme === 'light' ?  'dark'  :  'light'
+  // }
+  // const languageInvertor =(language) => {
+  //  return  language === 'english' ?  'ukrainian'  :  'english'
+  // }
+
+  // const themeMaker =() => {
+  //   localStorage.setItem('theme', themeInvertor(theme))
+    // dispatch(toggleTheme())
+  // }
+  // const languageMaker =() => {
+  //   localStorage.setItem('language', languageInvertor(language))
+  //   dispatch(toggleLang())
+  // }
+
   return (
-    <div>AppBar</div>
-  )
-}
+    <StyledHeader >
+      <Navigation />
+
+      <LangBtn  
+      // onClick={languageMaker}
+      type="button">
+      {(localStorage.getItem('language' ) || language) === 'english' ?  'EN' :  'UA'}
+      </LangBtn>
+
+      <ThemeBtn 
+      onClick={themeMaker}
+      type="button"
+    
+      >
+        {(localStorage.getItem('theme') || theme) === 'light'
+        ? <MdOutlineLightMode size={30}/>
+        : <MdOutlineNightlight size={30}/>
+        }
+      </ThemeBtn>
+      
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    </StyledHeader>
+  );
+};
 
 export default AppBar
