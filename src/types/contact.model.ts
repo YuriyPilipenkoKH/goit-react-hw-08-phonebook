fa7a960dd140125a2419ef1a725bfabf1d4026d4
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-15 04:41:28 +0200

    contactSchema zod

---
+++ src/types/contact.model.ts
@@ -0,0 +1,27 @@
+import z from 'zod'
+
+export const contactSchema = z.object({
+   _id: z
+  .string(),
+  name: z
+  .string()
+  .trim()
+  .min(3)
+  .max(32)
+  .refine((val) => !val.toLowerCase().startsWith('qwe'), {
+    message: 'forbidden prefix',
+  }),
+  number: z
+  .string()
+  .trim()
+  .regex(/^0\d{9}$/, {
+    message: 'Correct format: 0985551204 ',
+   })  ,
+  createdAt: z
+  .date()
+  .optional(),
+  updatedAt: z
+  .date()
+  .optional(),
+}) 
+export type Contact =  z.infer<typeof contactSchema>
