ad851fca66c723f9051074df640e71917d2bd075
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-16 20:51:16 +0200

    refactor axios utility; implement API configuration and enhance token refresh handling

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,46 +1,45 @@
-// import axios from "axios";
+import axios from "axios";
+import { getDispatch } from "../redux/store";
+import { logOut } from "../redux/auth/operations";
 
-// const HOST = import.meta.env.VITE_HOST
+const HOST = import.meta.env.VITE_HOST
 
-// const api = axios.create({
+const api = axios.create({
   
-//   baseURL:  `${HOST}`,
-//   withCredentials:true,
-//   headers: { "Content-Type": "application/json" }
-// })
+  baseURL:  `${HOST}`,
+  withCredentials:true,
+  headers: { "Content-Type": "application/json" }
+})
 
-// // Axios interceptor for handling expired tokens
-// api.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     const originalRequest = error.config;
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
 
-//     // If the access token is expired or invalid
-//     if (error.response?.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
 
-//       const refreshToken = localStorage.getItem("refreshToken");
-//       if (!refreshToken) {
-//         logoutUser();
-//         return Promise.reject(error);
-//       }
+      const refreshToken = localStorage.getItem("refreshToken");
+      if (!refreshToken) {
+        getDispatch()(logOut()); // Dispatch Redux action outside a component
+        return Promise.reject(error);
+      }
 
-//       try {
-//         const { data } = await axios.post("/refresh", { refreshToken });
-//         localStorage.setItem("accessToken", data.accessToken);
+      try {
+        const { data } = await axios.post("/refresh", { refreshToken });
+        localStorage.setItem("accessToken", data.accessToken);
 
-//         // Retry the original request with the new token
-//         originalRequest.headers["Authorization"] = `Bearer ${data.accessToken}`;
-//         return api(originalRequest);
-//       } catch (refreshError) {
-//         console.log("Refresh token expired or invalid. Logging out.");
-//         logoutUser();
-//         return Promise.reject(refreshError);
-//       }
-//     }
+        originalRequest.headers["Authorization"] = `Bearer ${data.accessToken}`;
+        return api(originalRequest);
+      } catch (refreshError) {
+        console.log("Refresh token expired or invalid. Logging out.");
+        getDispatch()(logOut());
+        return Promise.reject(refreshError);
+      }
+    }
 
-//     return Promise.reject(error);
-//   }
-// );
+    return Promise.reject(error);
+  }
+);
 
-// export default api;
+export default api;
