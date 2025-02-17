9470f75b2de373ec9ee965183efbc3ddbdc718af
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-17 09:33:01 +0200

    add TypeScript interface for MainModal props

--- src/components/modals/MainModal.tsx
+++ src/components/modals/MainModal.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const MainModal = () => {
+interface MainModalProps {
+  modalTypes: ModalBaseTypes
+  id: string
+  name: string
+}
+
+const MainModal: React.FC<MainModalProps> = () => {
   return (
     <div>MainModal</div>
   )
