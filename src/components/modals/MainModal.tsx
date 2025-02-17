5e3d30913cd8f7c17eaef01e7b9314f8db167cff
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-17 09:46:14 +0200

    modal root

--- src/components/modals/MainModal.tsx
+++ src/components/modals/MainModal.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 interface MainModalProps {
-  modalTypes: ModalBaseTypes
+  // modalTypes: ModalBaseTypes
   id: string
   name: string
 }
+const modalRoot = document.querySelector('#modal-root');
 
 const MainModal: React.FC<MainModalProps> = () => {
   return (
