8d69544967b28195184daa0e0a6dd154843dd29f
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-17 08:42:23 +0200

    applied hookform

--- src/components/forms/EditContactForm.tsx
+++ src/components/forms/EditContactForm.tsx
@@ -1,6 +1,29 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { addContactSchema, addContactSchemaType } from '../../types/AddComtact.model'
 
 const EditContactForm = () => {
+    const {
+      register, 
+      handleSubmit,
+      formState,
+      reset,
+      setError,
+    } = useForm<addContactSchemaType >({
+      defaultValues: {  
+        name: '',
+        number: '',
+         },
+          mode:'all',
+          resolver: zodResolver(addContactSchema), })
+      const {
+        errors,
+        isDirty,
+        isValid ,
+        isSubmitting,
+        isLoading 
+      } = formState
   return (
     <div>EditContactForm</div>
   )
