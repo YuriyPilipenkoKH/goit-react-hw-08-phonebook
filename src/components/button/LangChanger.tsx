568ad5c1136a9bd59f473e3dd2e6cfc984bd6e90
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-19 11:28:21 +0200

    LangChanger completed

--- src/components/button/LangChanger.tsx
+++ src/components/button/LangChanger.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAll } from '../../hooks/useAll'
+import { useAppDispatch } from '../../hooks/useAppDispatch'
+import { toggleLang } from '../../redux/langSlice'
 
 const LangChanger = () => {
+    const {language} = useAll()
+    const dispatch = useAppDispatch()
+
+      useEffect(() => {
+        localStorage.setItem('language', language);
+        document.documentElement.setAttribute('data-lang', language);
+      }, [language]);
+
   return (
-    <div>LangChanger</div>
+      <button
+        className="btn btn-ghost text-[var(--text-color)]"
+        onClick={()=>dispatch(toggleLang())}
+        aria-label="Toggle Theme"
+      >
+        {language === 'english' ? 'en' : 'ua'}
+      </button>
   )
 }
 
