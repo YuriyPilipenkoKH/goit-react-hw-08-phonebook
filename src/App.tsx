a4b84f58bb8dc614e1c52540a7779b719a7a6569
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-19 11:42:13 +0200

    clearde app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { Container } from './components/container/Container'
 import Layout from './components/layout/Layout'
-import { useSelector } from 'react-redux'
-import { getLang, getTheme } from './redux/selectors/selectors'
 import { useAppDispatch } from './hooks/useAppDispatch'
 import { lazy, useEffect } from 'react'
 import { refreshUser } from './redux/auth/operations'
@@ -19,12 +17,6 @@ const NotFoundPage = lazy(() => import('./pages/NotFoundPage '))
 function App() {
   const {token, user} = useAuth()
   const isAnmin = user?.role  === 'admin'
-
-  // const theme = useSelector(getTheme)
-  const language = useSelector(getLang)
-    // Set the data-theme attribute on the <html> element
-  // document.documentElement.setAttribute('data-theme', localStorage.getItem('theme') || theme);
-  document.documentElement.setAttribute('data-lang',  localStorage.getItem('language')  || language);
 
   const dispatch = useAppDispatch();
   useEffect(() => {
