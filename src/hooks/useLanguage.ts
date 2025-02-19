519575ec5f54ba74e392b482786087a82330d267
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-19 08:35:27 +0200

    hooc completed

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getLang } from "../redux/selectors/selectors";
 import { langEN, langUA } from "../utils/languages";
+import { languageTypes } from "../types";
 
 
 
--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getTheme } from "../redux/selectors/selectors";
 import { themeTypes } from "../types";
@@ -8,5 +8,11 @@ import { themeTypes } from "../types";
 export const Theme = () => {
   const [theme, setTheme] = useState<themeTypes>('dark');
   const language = useSelector(getTheme);
+
+      useEffect(() => {
+        setTheme(theme === 'light' ? "light" :  "dark");
+      }, [theme])
+
+      return theme
 
 }
