35f77ce6a23645e53fb0158442d26896a2991ac1
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-21 08:18:59 +0200

    translated global msg

--- src/components/phonebook/message/Message.tsx
+++ src/components/phonebook/message/Message.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Wrapper } from './Message.styled'
+import { useLanguage } from '../../../hooks/useLanguage'
 
 interface MessageProps{
   text: string
 }
 
 const Message: React.FC<MessageProps> = ({text}) => {
+    const lang = useLanguage()
   return (
     <div style={{width: '100%'}}>
-      <Wrapper>{text}</Wrapper>
+      <Wrapper>{lang[text]}</Wrapper>
     </div>
   )
 }
